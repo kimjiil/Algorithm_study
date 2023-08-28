9c5bf2ab006779fb9b642856e4b202174dc8df6c
kimjiil <kimjiil2013@naver.com>
2023-08-28 09:46:53 +0900

    [Bronze II] Title: 완전제곱수, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/1977. 완전제곱수/완전제곱수.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL)->sync_with_stdio(false);
+
+	int N, M;
+	cin >> N >> M;
+
+	
+	int left = int(ceil(pow(N, 0.5)));
+	int right = int(floor(pow(M, 0.5)));
+	int sum = 0;
+	for (int i = left; i <= right; i++) {
+		sum = sum + pow(i, 2);
+	}
+	if (sum == 0) {
+		cout << -1;
+	}
+	else {
+		cout << sum << "\n" << pow(left, 2) << endl;
+	}
+	
+	return 0;
+}

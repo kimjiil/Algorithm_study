9cfadeb70bb5e911104bbf21ccff41fdab9fb839
kimjiil <kimjiil2013@naver.com>
2023-08-28 09:34:40 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL)->sync_with_stdio(false);
+    
+    cout << "\\\    /\\\ \n";
+    cout << " )  ( ')\n";
+    cout << "(  /  )\n";
+    cout << " \\\(__)|\n";
+    
+    
+    
+}

723adacf5dcde7cf74198b542e6c592d3c078ae4
kimjiil <kimjiil2013@naver.com>
2023-08-28 09:47:55 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,16 @@
+#include <ios>
+
+main()
+{
+    int a, b;
+    scanf("%d%d", &a, &b);
+    if (a < b){
+        printf("<");
+    }
+    else if(a > b){
+        printf(">");
+    }
+    else{
+        printf("==");
+    }
+}

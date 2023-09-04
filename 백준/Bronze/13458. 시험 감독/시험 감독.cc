bcef098f7594558bf53f16ef7546d2af82d57269
kimjiil <kimjiil2013@naver.com>
2023-09-04 17:28:21 +0900

    [Bronze II] Title: 시험 감독, Time: 116 ms, Memory: 5804 KB -BaekjoonHub

--- 백준/Bronze/13458. 시험 감독/시험 감독.cc
+++ 백준/Bronze/13458. 시험 감독/시험 감독.cc
@@ -7,10 +7,9 @@ using namespace std;
 int main() {
 	cin.tie(NULL)->sync_with_stdio(false);
 	int N, B, C;
-	int* A;
+	int A[1000000];
 	cin >> N;
 
-	A = new int[N];
 	for (int i = 0; i < N; i++) {
 		cin >> A[i];
 	}
@@ -18,11 +17,11 @@ int main() {
 	cin >> B >> C;
 	ull com = 0;
 	for (int i = 0; i < N; i++) {
-		A[i] = A[i] - B;
+		//A[i] = A[i] - B;
 		com++;
-		if (A[i] > 0) {
-			com += (A[i] / C);
-			if (A[i] % C > 0) {
+		if (A[i] - B > 0) {
+			com += ((A[i] - B) / C);
+			if ((A[i] - B) % C > 0) {
 				com++;
 			}
 		}

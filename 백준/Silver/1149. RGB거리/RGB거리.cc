f635ef4ce741e6997d0a0b4714f6b00774e56c20
kimjiil <kimjiil2013@naver.com>
2023-09-14 17:50:37 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -8,26 +8,31 @@ int main() {
     
     int n;
     cin >> n;
-    int ** rgb = new int*[n];
-    // int ** dp = new int[n];
+    int rgb[3];
+    int dp[3];
+    int tmp[3];
     for(int i = 0 ; i < n ; i ++) {
-        rgb[i] = new int[3];
-        // dp[i] = new int[3];
         for(int j = 0 ; j < 3 ; j ++) {
-            cin >> rgb[i][j];
-            // dp[i][j] =
+            cin >> rgb[j]; //현재값
+            if(i == 0) {
+                dp[j] = rgb[j];
+            }
         }
-    }
-    for(int i = 1 ; i < n ; i++) {
-        rgb[i][0] = min(rgb[i-1][1], rgb[i-1][2]) + rgb[i][0];
-        rgb[i][1] = min(rgb[i-1][0], rgb[i-1][2]) + rgb[i][1];
-        rgb[i][2] = min(rgb[i-1][0], rgb[i-1][1]) + rgb[i][2];
+        
+        if(i != 0) {
+            for(int j = 0; j < 3; j++) {
+                tmp[j] = dp[j];
+            }
+            dp[0] = min(tmp[1], tmp[2]) + rgb[0];
+            dp[1] = min(tmp[0], tmp[2]) + rgb[1];
+            dp[2] = min(tmp[0], tmp[1]) + rgb[2];
+        }
     }
     
     int min_value = 1000 * 1000 + 1;
     for(int j = 0 ; j < 3 ; j++) {
-        if (min_value > rgb[n-1][j]) {
-            min_value = rgb[n-1][j];
+        if (min_value > dp[j]) {
+            min_value = dp[j];
         }
     }
     
